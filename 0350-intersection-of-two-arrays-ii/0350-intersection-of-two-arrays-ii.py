7ae3eccca68b70b338fd75d4deecb4b3b83eda39
Divyansh1217 <109619921+Divyansh1217@users.noreply.github.com>
2023-06-05 12:37:39 +0530

    Time: 54 ms (72.27%), Space: 16.5 MB (10.8%) - LeetHub

---
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
@@ -0,0 +1,11 @@
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        hashMap,result = {},[]
+        for num in nums1:hashMap[num] = hashMap.get(num,0)+1
+        
+        for num in nums2:
+            if num in hashMap and hashMap[num] != 0:
+                result.append(num) 
+                hashMap[num] -= 1
+
+        return result 
