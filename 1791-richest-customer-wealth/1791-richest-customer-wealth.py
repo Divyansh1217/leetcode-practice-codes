57cbf72a36d55479273558f7ad013e1d649389cf
Divyansh1217 <109619921+Divyansh1217@users.noreply.github.com>
2023-10-06 19:52:01 +0530

    Time: 60 ms (47.6%), Space: 16.1 MB (94.96%) - LeetHub

---
+++ 1791-richest-customer-wealth/1791-richest-customer-wealth.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        a=[]
+        for i in range(len(accounts)):
+            a.append(sum(accounts[i]))
+        return max(a)
+
+        
