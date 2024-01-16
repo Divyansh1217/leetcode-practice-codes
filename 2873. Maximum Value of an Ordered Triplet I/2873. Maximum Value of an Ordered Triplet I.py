11af84dee2a39993cd8cf86e4e202a8e3a2d1b85
Divyansh1217 <109619921+Divyansh1217@users.noreply.github.com>
2024-01-16 19:54:15 +0530

    Create 2873. Maximum Value of an Ordered Triplet I.py

---
+++ 2873. Maximum Value of an Ordered Triplet I/2873. Maximum Value of an Ordered Triplet I.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maximumTripletValue(self, nums: List[int], mx = 0) -> int:
+
+        pref = accumulate(nums, max)
+        suff = list(accumulate(nums[::-1], max))[::-1]
+
+        return max(0,*((J-I) * K for I, J, K 
+                                  in zip(nums[1:],pref,suff[2:])))
