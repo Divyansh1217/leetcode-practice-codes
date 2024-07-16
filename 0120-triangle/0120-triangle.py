359049e8e9598114729651b284eb197fd9533a2c
Divyansh_Aggarwal <109619921+Divyansh1217@users.noreply.github.com>
2024-07-16 11:17:54 +0530

    Time: 68 ms (22.02%), Space: 20.3 MB (5.59%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,8 +1,6 @@
 class Solution:
     def minimumTotal(self, a: List[List[int]]) -> int:
-        n=len(a)
-        dp=[[0] * (n+1) for _ in range(n+1)]
-        for level in range(n-1,-1,-1):
-            for i in range(level+1):
-                dp[level][i]=a[level][i] + min(dp[level+1][i], dp[level+1][i+1])
-        return dp[0][0]  
+        @cache
+        def dfs(level, i):            
+            return 0 if level >= len(a) else a[level][i] + min(dfs(level + 1, i), dfs(level + 1, i+1))        
+        return dfs(0, 0)            
